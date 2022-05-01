cb7f69cfa17d64949324aeafc046426691793958
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-05-01 23:30:57 +0700

    feat: add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import Layout from 'components/layout';
+import { AllProjects } from 'components/projects';
+
+const Projects = () => {
+  return (
+    <Layout title="Projects">
+      <AllProjects />
+    </Layout>
+  );
+};
+
+export default Projects;
