fa24bf7d29b26aa94c37365ddd81752075184cc7
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-05-01 23:29:54 +0700

    refactor(types): adjust project data types

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,14 +1,16 @@
-interface CreatorData {
-  id: string;
+import { IconType } from 'react-icons';
+
+interface TechStackData {
   name: string;
-  github_link: string;
+  icon: IconType;
 }
 
 export interface ProjectData {
   id: string;
   name: string;
   description: string;
-  other_creators: CreatorData[];
+  type: 'Personal' | 'Group';
+  tech_stack: TechStackData[];
   icon_path: string;
   preview_path: string;
   link: string;
