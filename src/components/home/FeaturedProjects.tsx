091cc8da1bf2760aafc3e8da7b8dd642d9fba19e
Muhammad Akmal Hisyam <74256288+akmalhisyammm@users.noreply.github.com>
2022-05-04 21:30:20 +0700

    feat(components): add animation in each component (#5)
    * refactor: change summary data type in about context
    
    * chore(pages): integrate framer motion
    
    * feat(components): add animation in each component

--- src/components/home/FeaturedProjects.tsx
+++ src/components/home/FeaturedProjects.tsx
@@ -12,7 +12,24 @@ import {
 } from '@chakra-ui/react';
 import { useContext } from 'react';
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
 import { ProjectContext } from 'contexts/project';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.4,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 50, opacity: 0 },
+  show: { y: 0, opacity: 1, transition: { ease: 'easeOut' } },
+};
 
 const FeaturedProjects = () => {
   const router = useRouter();
@@ -21,61 +38,70 @@ const FeaturedProjects = () => {
 
   return (
     <Box marginY={8}>
-      <Heading as="h3" fontSize={24}>
-        Featured Projects
-      </Heading>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: 'easeOut', delay: 0.2 }}>
+        <Heading as="h3" fontSize={24}>
+          Featured Projects
+        </Heading>
+      </motion.div>
 
-      <SimpleGrid columns={[1, 2, 2]} gap={6} marginY={4}>
-        {projectsCtx.projects.slice(0, 4).map((project) => (
-          <ChakraLink key={project.id} href={project.link} isExternal>
-            <Box
-              borderRadius={18}
-              overflow="hidden"
-              position="relative"
-              borderWidth={2}
-              height={200}
-              cursor="pointer"
-              transition="0.2s ease-out"
-              _hover={{
-                transform: 'scale(1.04)',
-                borderWidth: '3px',
-                borderColor: colorMode === 'light' ? 'blue.500' : 'blue.200',
-              }}>
-              <Image
-                src={project.preview_path}
-                alt={project.name + ' Preview'}
-                height="full"
-                objectFit="cover"
-              />
-              <VStack
-                position="absolute"
-                justifyContent="end"
-                alignItems="start"
-                bottom={0}
-                padding={4}
-                width="full"
-                height="full"
-                background="linear-gradient(360deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.2) 40%, rgba(0, 0, 0, 0) 100%)">
-                <HStack position="absolute" spacing={3}>
-                  <Image src={project.icon_path} alt={project.name + ' Icon'} width={8} />
-                  <Text color="white" fontWeight={600}>
-                    {project.name}
-                  </Text>
-                </HStack>
-              </VStack>
-            </Box>
-          </ChakraLink>
-        ))}
-      </SimpleGrid>
+      <motion.div variants={container} initial="hidden" animate="show">
+        <SimpleGrid columns={[1, 2, 2]} gap={6} marginY={4}>
+          {projectsCtx.projects.slice(0, 4).map((project) => (
+            <motion.div key={project.id} variants={item}>
+              <ChakraLink href={project.link} isExternal>
+                <Box
+                  borderRadius={18}
+                  overflow="hidden"
+                  position="relative"
+                  borderWidth={2}
+                  height={200}
+                  cursor="pointer"
+                  transition="0.2s ease-out"
+                  _hover={{
+                    transform: 'scale(1.04)',
+                    borderWidth: '3px',
+                    borderColor: colorMode === 'light' ? 'blue.500' : 'blue.200',
+                  }}>
+                  <Image
+                    src={project.preview_path}
+                    alt={project.name + ' Preview'}
+                    height="full"
+                    objectFit="cover"
+                  />
+                  <VStack
+                    position="absolute"
+                    justifyContent="end"
+                    alignItems="start"
+                    bottom={0}
+                    padding={4}
+                    width="full"
+                    height="full"
+                    background="linear-gradient(360deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.2) 40%, rgba(0, 0, 0, 0) 100%)">
+                    <HStack position="absolute" spacing={3}>
+                      <Image src={project.icon_path} alt={project.name + ' Icon'} width={8} />
+                      <Text color="white" fontWeight={600}>
+                        {project.name}
+                      </Text>
+                    </HStack>
+                  </VStack>
+                </Box>
+              </ChakraLink>
+            </motion.div>
+          ))}
+        </SimpleGrid>
 
-      <Button
-        colorScheme="blue"
-        variant="outline"
-        borderRadius="full"
-        isFullWidth
-        onClick={() => router.replace('/projects')}>
-        View all projects
-      </Button>
+        <Button
+          colorScheme="blue"
+          variant="outline"
+          borderRadius="full"
+          isFullWidth
+          onClick={() => router.replace('/projects')}>
+          View all projects
+        </Button>
+      </motion.div>
     </Box>
   );
 };
