3e8c09839c796b64a48f0c470a04b112d8e016bc
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-05-04 10:56:58 +0700

    feat(components): add social media in footer section

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,48 @@
-import { Box, Link as ChakraLink, Text } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Icon,
+  Link as ChakraLink,
+  Text,
+  Tooltip,
+  useColorMode,
+} from '@chakra-ui/react';
+import { useContext } from 'react';
 import Link from 'next/link';
+import { AboutContext } from 'contexts/about';
 
 const Footer = () => {
+  const aboutCtx = useContext(AboutContext);
+  const { colorMode } = useColorMode();
+
   return (
-    <Box as="footer" position="relative" width="full" textAlign="center">
+    <Box
+      as="footer"
+      position="relative"
+      width="full"
+      textAlign="center"
+      borderTop="1px solid"
+      paddingY={4}>
+      <HStack justifyContent="center" spacing={3}>
+        {aboutCtx.about.social_media.map((sm) => (
+          <Tooltip key={sm.name} label={sm.name}>
+            <ChakraLink href={sm.link} isExternal>
+              <Icon
+                as={sm.icon}
+                fontSize={18}
+                _hover={{ color: colorMode === 'light' ? 'blue.500' : 'blue.200' }}
+              />
+            </ChakraLink>
+          </Tooltip>
+        ))}
+      </HStack>
+
       <Text>
         2022 &bull;{' '}
         <Link href="/about" passHref>
-          <ChakraLink>Muhammad Akmal Hisyam</ChakraLink>
+          <ChakraLink _hover={{ color: colorMode === 'light' ? 'blue.500' : 'blue.200' }}>
+            Muhammad Akmal Hisyam
+          </ChakraLink>
         </Link>
       </Text>
     </Box>
