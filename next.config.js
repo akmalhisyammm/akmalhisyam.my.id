29c329312fdc3347797cc26d52cf133eeab963f6
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-07-19 21:44:03 +0700

    chore: add next-i18next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
+const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
@@ -13,4 +14,5 @@ module.exports = withPWA({
   images: {
     domains: ['https://akmalhisyam.s3.ap-southeast-1.amazonaws.com'],
   },
+  i18n,
 });
