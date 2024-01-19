37a8fa7d0baede79bd29cf9106960aaf06b4ae0b
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2024-01-19 21:59:02 +0700

    chore: update dependencies and setup some configurations

---
+++ i18n.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  locales: ['en', 'id'],
+  defaultLocale: 'en',
+};
--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
-const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['https://akmalhisyam.s3.ap-southeast-1.amazonaws.com'],
+    domains: [`${process.env.NEXT_PUBLIC_IMAGE_URL}`],
   },
-  i18n,
 });
