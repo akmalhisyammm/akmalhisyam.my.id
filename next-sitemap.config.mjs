7d922a54a1e06ca550e503b1efa1eba8d8801b12
Muhammad Akmal Hisyam <74256288+akmalhisyammm@users.noreply.github.com>
2024-06-30 07:22:10 +0700

    chore: setup cross env, remove unused deps, and adjust next sitemap (#25)

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const nextSitemapConfig = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
   generateRobotsTxt: true,
 };
 
